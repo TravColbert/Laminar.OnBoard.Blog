822728bafd938c70dc7284370b2ad0b8447fee01
TravColbert <trav.colbert@gmail.com>
2018-11-20 07:49:29 -0800

    repo cleanup

--- modules/Blog.js
+++ modules/Blog.js
@@ -3,25 +3,35 @@ module.exports = function(app) {
     home: function(req,res,next) {
       return new Promise((resolve,reject) => {
         let myName = "BlogHome";
+        // If the user is not logged-in only get 'public' notes
+        // If the user is logged-in get **'your'** notes plus public notes of everybody else
         let searchObj = {
-          where: {
-            name: "Default"
+          where:{
+            public: true
           },
-          include: [app.models["notes"]]
+          include: [
+            {
+              model: app.models["domains"],
+              where: {
+                public: true
+              },
+              as:"domain"
+            },
+            {
+              model: app.models["users"],
+              as:"user"
+            }
+          ]  
         }
         req.appData.notes = [];
-        app.controllers["domains"].__get(searchObj)
-        .then((result) => {
-          let notes = [];
-          result.forEach(domain => {
-            app.log("Getting notes for domain: " + domain.name + " (" + domain.notes.length + ")",myName,6);
-            notes = notes.concat(domain.notes);
-          });
+        app.controllers["notes"].__get(searchObj)
+        .then((notes) => {
+          app.log(notes,myName,6);
           req.appData.notes = notes;
           req.appData.view = "bloghome";
           resolve(true);
-        });
-      });
+        })
+      })
     }
   }
 }
