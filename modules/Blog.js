c82eab7a2189b65abbfc48441cf5aa3be82a8376
TravColbert <trav.colbert@gmail.com>
2018-11-28 08:02:04 -0800

    update blog layouts

--- modules/Blog.js
+++ modules/Blog.js
@@ -3,34 +3,36 @@ module.exports = function(app) {
     home: function(req,res,next) {
       return new Promise((resolve,reject) => {
         let myName = "BlogHome";
-        // If the user is not logged-in only get 'public' notes
-        // If the user is logged-in get **'your'** notes plus public notes of everybody else
-        let searchObj = {
-          where:{
-            public: true
-          },
-          include: [
-            {
-              model: app.models["domains"],
-              where: {
-                public: true
-              },
-              as:"domain"
-            },
-            {
-              model: app.models["users"],
-              as:"user"
-            }
-          ]  
-        }
         req.appData.notes = [];
-        app.controllers["notes"].__get(searchObj)
-        .then((notes) => {
-          app.log(notes,myName,6);
-          req.appData.notes = notes;
+        app.log("Retrieving public notes",myName,6);
+        app.controllers["notes"].getPublicNotes()
+        .then(notes => {
+          app.log("Got " + notes.length + " notes",myName,6);
+          return (req.appData.notes = req.appData.notes.concat(notes));
+        })
+        .then(() => {
+          if(req.session.user) {
+            return app.controllers["notes"].getNotesByUserId(req.session.user.id)
+          } else {
+            return [];
+          }
+        })
+        .then(notes => {
+          app.log("Got " + notes.length + " user's notes",myName,6);
+          let noteIds = req.appData.notes.map(note => {
+            return note.id;
+          });
+          for(let note of notes) {
+            if(noteIds.indexOf(note.id)<0) req.appData.notes.push(note);
+          }
+          req.appData.notes.sort(function(a,b){
+            if(a.updatedAt < b.updatedAt) return 1;
+            if(a.updatedAt > b.updatedAt) return -1;
+            return 0;
+          });
           req.appData.view = "bloghome";
           resolve(true);
-        })
+        });
       })
     }
   }
