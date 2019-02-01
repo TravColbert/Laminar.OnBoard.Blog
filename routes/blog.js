aef4b062363887a779342780852b710166616dd1
TravColbert <trav.colbert@gmail.com>
2019-02-01 14:45:09 -0800

    enabled blog entry

--- routes/blog.js
+++ routes/blog.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 module.exports = function (app) {
   router.get('/', app.tools.homePage)
-  // router.get('/:id/',app.controllers["notes"].getAsBlog);
+  router.get('/:id/', app.controllers['notes'].getAsBlog)
   router.get('/:domainId/articles/', app.controllers['notes'].getNotesInDomain)
   router.get('/:domainId/articles/:id/', app.controllers['notes'].getAsBlog)
   router.get('/:id/actions/edit', app.tools.checkAuthentication, app.controllers['notes'].editNoteForm)
