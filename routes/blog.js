f6aca1f614a03204acbcd648870cebfcffe56de6
TravColbert <trav.colbert@gmail.com>
2019-06-03 15:10:46 -0700

    implementing trailing slashes

--- routes/blog.js
+++ routes/blog.js
@@ -1,5 +1,5 @@
 const express = require('express')
-var router = express.Router()
+var router = express.Router({ strict: true })
 
 module.exports = function (app) {
   // router.get('/', app.tools.homePage)
