1da8bfc269716edfa95731823641bab2906cc207
TravColbert <trav.colbert@gmail.com>
2019-10-17 09:52:55 -0700

    added route and view support for blog slugs

--- routes/blog.js
+++ routes/blog.js
@@ -4,7 +4,7 @@ var router = express.Router({ strict: true })
 module.exports = function (app) {
   // router.get('/', app.tools.homePage)
   router.get('/', app.controllers['notes'].gets, app.controllers['notes'].showBlog)
-  router.get('/:id/', app.controllers['notes'].getAsBlog)
+  router.get('/:slug/', app.controllers['notes'].getBlogBySlug)
   router.get('/:domainId/articles/', app.controllers['notes'].getNotesInDomain)
   router.get('/:domainId/articles/:id/', app.controllers['notes'].getAsBlog)
   router.get('/:id/actions/edit', app.tools.checkAuthentication, app.controllers['notes'].editNoteForm)
