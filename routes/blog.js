bdc6c853f4246258e60d7ca78c47290860b5b622
TravColbert <trav.colbert@gmail.com>
2019-11-15 16:41:29 +0200

    support for blog/notes list by domain name

--- routes/blog.js
+++ routes/blog.js
@@ -5,6 +5,7 @@ module.exports = function (app) {
   // router.get('/', app.tools.homePage)
   router.get('/', app.controllers['notes'].gets, app.controllers['notes'].showBlog)
   router.get('/:slug/', app.controllers['notes'].getBlogBySlug)
+  router.get('/:domainName/', app.controllers['notes'].getNotesByDomainName)
   router.get('/:domainId/articles/', app.controllers['notes'].getNotesInDomain)
   router.get('/:domainId/articles/:id/', app.controllers['notes'].getAsBlog)
   router.get('/:id/actions/edit', app.tools.checkAuthentication, app.controllers['notes'].editNoteForm)
